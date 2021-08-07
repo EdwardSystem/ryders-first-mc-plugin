af6e9afdf6c75687c3a2ab7363f7816b7909e172
BuildTools <unconfigured@null.spigotmc.org>
2021-08-08 07:12:27 +1000

    Added new startup and shutdown message. Removed old 'sout' command and replaced with bukkit integration.

--- src/main/java/com/edwardsystems/rydersfirstplugin/RydersFirstPlugin.java
+++ src/main/java/com/edwardsystems/rydersfirstplugin/RydersFirstPlugin.java
@@ -1,5 +1,6 @@
 package com.edwardsystems.rydersfirstplugin;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RydersFirstPlugin extends JavaPlugin {
@@ -7,12 +8,13 @@ public final class RydersFirstPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        getLogger().info("TEST WE R COOL");
-        getLogger().info("HELLO RYDER STOINLKS");
+        getServer().getConsoleSender().sendMessage(ChatColor.DARK_AQUA + "[RydersFirstPlugin] Successful boot. Standby.");
+
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getServer().getConsoleSender().sendMessage(ChatColor.DARK_AQUA + "[RydersFirstPlugin] Successful shutdown. Goodbye.");
     }
 }
